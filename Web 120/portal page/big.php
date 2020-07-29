6292da8b2edd60be8b774fa54c5921027e40f592
john-jacobsen <58801849+john-jacobsen@users.noreply.github.com>
2020-07-28 23:14:20 -0500

    upload of initial web 120 files
    Does not include CONFIG file as it includes re-CAPTCHA credentials

---
+++ Web 120/portal page/big.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+	<meta charset="utf-8">
+	<title>BIG</title>
+	<meta name="author" content="Your Name">
+	<meta name="description" content="Example description">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://use.fontawesome.com/6a71565c22.js"></script>
+	<link rel="stylesheet" href="">
+	<link rel="icon" type="image/x-icon" href=""/>
+</head>
+
+<body>
+	<header>
+         <h1><a href="index.php"><i class="logo fa fa-home"></i> John Jacobsen SCC WEB120 Portal</a></h1>
+      <nav>
+        <ul class="topnav" id="myTopnav">
+          <li><a href="index.php" class="selected">Welcome</a></li>
+          <li><a href="big/index.php">Big</a></li>
+          <li><a href="aia.php">AIA</a></li>
+          <li><a href="flowchart.php">Flowchart/Layout</a></li>
+          <li><a href="fp/index.php">Final Project</a></li>
+          <li><a href="contact.php">Contact Johnny</a></li>
+          <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+        </ul>
+      </nav>
+    </header>
+	<main></main>
+	<footer>
+        <p><small>&copy; 2018 - <?=date('Y')?> by <a href="contactme.php" target="_blank">Johnny</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+    </footer>
+	<script type="text/javascript" src=""></script>
+</body>
+    
+</html>
